9559afd869db0f007d68e1544ff545acdca8e1da
Céline Eap <celine-chhun-huy.eap@edu.devinci.fr>
2025-06-11 16:32:07 +0200

    :sparkles: UPDATE profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,21 +2,36 @@
 
 namespace App\Controller;
 
+use App\Form\ProfileForm;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         if (!$this->getUser()) {
             return $this->redirectToRoute('app_login');
         }
 
+        $profile = $this->getUser();
+        $form = $this->createForm(ProfileForm::class, $profile);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($profile);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_profile');
+        }
+
         return $this->render('profile/index.html.twig', [
-            'profile' => $this->getUser(),
+            'profile' => $profile,
+            'profileForm' => $form,
         ]);
     }
 }
