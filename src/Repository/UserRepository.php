b2425f3a9cadceb931491c0ce7ee1eafeb622623
Céline Eap <celine-chhun-huy.eap@edu.devinci.fr>
2025-06-15 13:21:08 +0200

    :adhesive_bandage: FIX

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -48,13 +48,12 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
 //        ;
 //    }
 
-//    public function findOneBySomeField($value): ?User
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    public function findAllAdmins(): array
+    {
+        return $this->createQueryBuilder('u')
+            ->where('u.roles LIKE :role')
+            ->setParameter('role', '%ROLE_ADMIN%')
+            ->getQuery()
+            ->getResult();
+    }
 }
